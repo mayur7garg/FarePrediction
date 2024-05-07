241d44ce1766a3b2c447caf24264bd3a99a314fb
Mayur Garg <mayur7garg@gmail.com>
2024-05-07 13:37:36 +0000

    Added parquet file converter

---
+++ src/01 Preprocessing data.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import polars as pl
+
+for raw_path in Path("..", "data", "raw").glob("**/*.csv"):
+    df = pl.read_csv(raw_path)
+    output_file_path = Path("..", "data", "processed", raw_path.parent.name, f"{raw_path.stem}.parquet")
+    output_file_path.parent.mkdir(exist_ok = True)
+
+    df.write_parquet(output_file_path)
+    print(df.shape, output_file_path)
