a934ddced03b1c09f4a6114da269d1a0ca4fb0ec
Mayur Garg <mayur7garg@gmail.com>
2024-05-08 19:14:51 +0000

    Added preprocessing notebook

--- src/01 Parquet Conversion.py
+++ src/01 Parquet Conversion.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import polars as pl
 
+# Convert all files to parquet to reduce file size for git
+
 for raw_path in Path("..", "data", "raw").glob("**/*.csv"):
     df = pl.read_csv(raw_path)
     output_file_path = Path("..", "data", "parquet", raw_path.parent.name, f"{raw_path.stem}.parquet")
